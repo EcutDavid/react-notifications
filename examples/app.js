9c1bf0c78e031a1e38bc8590c5d460b725e0e273
David Guan <EcutDavid@users.noreply.github.com>
2017-04-17 21:45:24 +0800

    implement basic functions (#2)

--- examples/app.js
+++ examples/app.js
@@ -2,10 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Demo from 'demo';
 
+const MOCK_NOTIFICATIONS = [{
+    type: 'info',
+    message: 'Hello'
+}, {
+    type: 'warning',
+    message: 'Hi'
+}, {
+    type: 'error',
+    message: 'Oooops'
+}];
+
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {notifications: MOCK_NOTIFICATIONS};
+    }
+
+    oncloseHandle(index) {
+        const { notifications } = this.state;
+
+        const newNotifications =
+            notifications.slice(0, index).concat(notifications.slice(index + 1));
+
+        this.setState({notifications: newNotifications});
+    }
+
     render() {
      return (
-         <Demo defaultExpand={true} />
+         <Demo
+             capacity={3}
+             notifications={this.state.notifications}
+             onClose={this.oncloseHandle.bind(this)}
+         />
      );
     }
 }
