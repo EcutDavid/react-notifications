33fee5bd1c6607385e3bfc206dd5c70988d13194
David Guan <DavidGuanDev@Gmail.com>
2017-04-17 21:44:15 +0800

    implement basic functions

---
+++ src/components/messageBar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import '../styles/messageBar.scss';
+
+export default class MessageBar extends Component {
+    render() {
+        const { type, message, onClose } = this.props;
+
+        return (
+            <div className='messageBar'>
+                <header className='message-header'>
+                    <p>{type}</p>
+                    <i
+                        className='close'
+                        onClick={() => onClose()}
+                    >
+                        +
+                    </i>
+                </header>
+                <article className='message-body'>
+                    {message}
+                </article>
+            </div>
+        )
+    }
+}
